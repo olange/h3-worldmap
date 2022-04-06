42deac1e9c99a30d76630015014f99695d08202d
Olivier Lange <olange@hey.com>
2022-04-07 01:12:53 +0200

    Adds `projection` and `areas` attributes

--- h3-worldmap.js
+++ h3-worldmap.js
@@ -4,12 +4,35 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {LitElement, html, css} from 'lit';
+import { LitElement, html, svg, css } from 'lit';
+import * as d3 from 'd3';
+import { h3IsValid } from 'h3-js';
+
+// Utility functions
+// TODO: move to separate module
+
+function removeDuplicates( arr) {
+  return [...new Set(arr)];
+}
+
+// Map of projection identifiers, which are the possible values of the 'projection'
+// attribute of our custom element, with their corresponding friendly name and
+// D3 projection constructor function.
+// TODO: complete list from https://observablehq.com/@d3/projection-comparison
+// TODO: move to separate module
+const AVAILABLE_PROJECTIONS = new Map([
+  [ "conicEqualArea", { id: "conicEqualArea", name: "Conic equal-area", ctorFn: d3.geoConicEqualArea } ],
+  [ "orthographic",   { id: "orthographic", name: "Orthographic", ctorFn: d3.geoOrthographic } ],
+  [ "naturalEarth",   { id: "naturalEarth", name: "Natural Earth", ctorFn: d3.geoNaturalEarth1 } ],
+  [ "stereographic",  { id: "stereographic", name: "Stereographic", ctorFn: d3.geoStereographic } ],
+  [ "gnomonic",       { id: "gnomonic", name: "Gnomonic", ctorFn: d3.geoGnomonic } ],
+  [ "mercator",       { id: "mercator", name: "Mercator", ctorFn: d3.geoMercator } ],
+]);
 
 /**
- * An example element.
+ * ‹h3-worldmap› custom element.
  *
- * @fires count-changed - Indicates when the count changes
+ * @fires (nothing) - Indicates (nothing)
  * @slot - This element has a slot
  * @csspart button - The button
  */
@@ -17,58 +40,148 @@ export class H3Worldmap extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: block;
-        border: solid 1px gray;
-        padding: 16px;
-        max-width: 800px;
+        display: inline-block;
+        --primary-color: black;
+        --secondary-color: #dddddd;
+        --tertiary-color: #cccccc;
+        --highlight-color: #990000;
+        --areas-color: #cc0000cc;
+        --bounding-box-color: #ddddddcc;
+        --background-color: white;
       }
+
+      .sphere { fill: var(--background-color); stroke: none; }
+      .outline { fill: none; stroke: var(--secondary-color); }
+      .land { fill: var(--primary-color); stroke: none; }
+      .graticule { fill: none; stroke: var(--secondary-color); }
+      .hexes { fill: none; stroke: var(--tertiary-color); stroke-width: 0.35; }
+      .areas { fill: var(--areas-color); stroke: var(--highlight-color); }
+      .bbox { fill: none; stroke: var(--highlight-color); }
+
+      .info {
+        color: var(--primary-color);
+        border: 3px solid var(--primary-color); border-radius: 1.5rem;
+        padding: 1rem 1.5rem;
+      }
+
+      code { font-size: 1.3rem; }
     `;
   }
 
   static get properties() {
     return {
       /**
-       * The name to say "Hello" to.
+       * Geodesic projection identifier (a string matching one of the D3-geo
+       * projections that we support — see AVAILABLE_PROJECTIONS map)
+       *
        * @type {string}
        */
-      name: {type: String},
+      projection: { type: String },
 
       /**
-       * The number of times the button has been clicked.
-       * @type {number}
+       * An array of H3-indexes, in JSON-stringified form.
+       *
+       * For instance:
+       *     <code>&lt;h3-worldmap
+       *        areas='[ 80abfffffffffff, 80a5fffffffffff ]'></code>
+       *
+       * @type {array}
        */
-      count: {type: Number},
+      areas: { type: Array },
     };
   }
 
   constructor() {
     super();
-    this.name = 'World';
-    this.count = 0;
+
+    // Default value of public attributes/properties
+    this.projection = "orthographic";    // will trigger its property setter
+    this.areas = [];                     // will trigger its property setter
+
+    // Internal private properties
+    this._width = 960;                   // TODO: compute width from <svg> element bbox
+    this._height = 480;                  // TODO: compute height from <svg> element bbox
+
+    this._uniqueAreas = undefined;        // computed from `this._areas` (see `willUpdate()`)
+    this._projectionDef = undefined;      // computed from `this._projection` (see `willUpdate()`)
+  }
+
+  set areas( arr) {
+    // Validates that the new value is an array, of valid H3-indexes
+    if( !Array.isArray( arr))
+      throw new TypeError( `Property 'areas' must contain an array of H3-indexes; got ${arr}`);
+    const anyInvalidArea = arr.find( area => !h3IsValid( area));
+    if( anyInvalidArea)
+      throw new TypeError( `Property 'areas' must contain valid H3-indexes; '${anyInvalidArea}' is an invalid H3-index`)
+    const oldAreas = this._areas;
+    this._areas = arr;
+    this.requestUpdate("areas", oldAreas);
+  }
+
+  set projection(id) {
+    // Validates that the new value is one of the supported projection identifiers
+    if( !AVAILABLE_PROJECTIONS.has( id))
+      throw new RangeError( `Unsupported 'projection' property value: '${id}'`);
+    const oldId = this._projection;
+    this._projection = id;
+    this.requestUpdate("projection", oldId);
+  }
+
+  willUpdate(changedProperties) {
+    // Compute derived properties
+    if (changedProperties.has('projection')) {
+      this._projectionDef = AVAILABLE_PROJECTIONS.get( this._projection);
+    }
+    if (changedProperties.has('areas')) {
+      this._uniqueAreas = removeDuplicates(this._areas);
+    }
   }
 
   render() {
+    return [
+      this.worldMap(this._width,this._height),
+      this.infoBox(this._uniqueAreas, this._projectionDef)
+    ];
+  }
+
+  infoBox(areas, projDef) {
     return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `;
+      <div class="info">
+        ${this.areasRepr(areas)}<br>
+        ${this.projectionRepr(projDef)}
+        <slot></slot>
+      </div>`;
   }
 
-  _onClick() {
-    this.count++;
-    this.dispatchEvent(new CustomEvent('count-changed'));
+  projectionRepr(projDef) {
+    return html`
+      <strong>${projDef?.name}</strong> projection (<code>${projDef?.id}</code>)`;
   }
 
-  /**
-   * Formats a greeting
-   * @param name {string} The name to say "Hello" to
-   * @returns {string} A greeting directed at `name`
-   */
-  sayHello(name) {
-    return `Hello, ${name}`;
+  areasRepr(areas) {
+    return html`
+      Areas (<em>H3-indexes</em>): [ <strong>${areas?.map(
+        (area,i) => html`${i > 0 ? ', ' : ''}<code>${area}</code>`)}</strong> ]`;
+  }
+
+  worldMap(width,height) {
+    return svg`
+      <svg class="map" viewBox="0 0 ${width} ${height}">
+        <circle cx="50" cy="50" r="40" class="outline" />
+      </svg>`;
+      // <defs>
+      //   <path id="outline" d="${this.pathFn(H3Worldmap.outlineGeom)}" />
+      //   <clipPath id="clip"><use href="#outline" /></clipPath>
+      // </defs>
+      // <g clip-path="#clip">
+      //   <use href="#outline" class="sphere" />
+      //   <!-- <path d="${this.pathFn(this.graticuleGeom)}" class="graticule" /> -->
+      //   <path d="${this.pathFn(this.hexesGeom)}" class="hexes" />
+      //   <path d="${this.pathFn(land)}" class="land" />
+      //   <path d="${this.pathFn(this.bsphereGeom)}" class="bbox" />
+      //   <path d="${this.pathFn(this.areasGeom)}" class="areas" />
+      // </g>
+      // <use href="#outline" class="outline" />
   }
 }
 
