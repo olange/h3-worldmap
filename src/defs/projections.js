069c88dc008ec185582d9feb7b85fbbebaf9ab69
Olivier Lange <olange@hey.com>
2022-05-02 12:14:24 +0200

    Adds a ‹D3 projection definition› class
    to have it easier to use/transform this record.

--- src/defs/projections.js
+++ src/defs/projections.js
@@ -1,6 +1,33 @@
 import * as d3 from 'd3';
 
 /**
+ * Projection definition object. Holds a human representation of
+ * the name of a D3-geo projection, along with its _builder function_
+ * from D3-geo.
+ *
+ * Note: a projection _builder function_ is a pattern from D3-geo,
+ * which declares projections as functions, that can be instantiated
+ * and later configured for a particular view (scale, center, rotate).
+ * The function object indeed holds various instance properties.
+ *
+ * The pattern isn't explicitely named as such within the docs of
+ * D3-geo, it is a convention of ours.
+ */
+class ProjDef {
+  constructor(name, ctorFn) {
+    if( typeof ctorFn !== "function") {
+      throw new TypeError( `Expected a projection function from D3-geo, got ${ctorFn}`)
+    }
+    this.name = name;
+    this.ctorFn = ctorFn;
+  }
+
+  toString() {
+    return `ProjDef { name: ${this.name}, builderFn: ${this.builderFn} }`;
+  }
+}
+
+/**
  * Map of the projection identifiers, which are the possible values of the
  * 'projection' attribute of our custom element, with their corresponding
  * friendly name and D3 projection constructor function.
@@ -12,11 +39,11 @@ import * as d3 from 'd3';
  * @default
  */
  export const AVAILABLE_PROJECTIONS = new Map([
-  [ "conicEqualArea", { name: "Conic equal-area", ctorFn: d3.geoConicEqualArea } ],
-  [ "orthographic",   { name: "Orthographic", ctorFn: d3.geoOrthographic } ],
-  [ "naturalEarth",   { name: "Natural Earth", ctorFn: d3.geoNaturalEarth1 } ],
-  [ "stereographic",  { name: "Stereographic", ctorFn: d3.geoStereographic } ],
-  [ "gnomonic",       { name: "Gnomonic", ctorFn: d3.geoGnomonic } ],
-  [ "mercator",       { name: "Mercator", ctorFn: d3.geoMercator } ],
+  [ "conicEqualArea", new ProjDef( "Conic equal-area", d3.geoConicEqualArea) ],
+  [ "orthographic",   new ProjDef( "Orthographic", d3.geoOrthographic) ],
+  [ "naturalEarth",   new ProjDef( "Natural Earth", d3.geoNaturalEarth1) ],
+  [ "stereographic",  new ProjDef( "Stereographic", d3.geoStereographic) ],
+  [ "gnomonic",       new ProjDef( "Gnomonic", d3.geoGnomonic) ],
+  [ "mercator",       new ProjDef( "Mercator", d3.geoMercator) ],
   // TODO: complete list from https://observablehq.com/@d3/projection-comparison
 ]);
